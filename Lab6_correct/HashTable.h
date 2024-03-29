f55153ed9a5fced092bfd0cf84b48944d2b19e9b
Евгений Андреев <evgenij1745@mail.ru>
2024-03-29 13:56:50 +0300

    added choosing the size of the table

--- Lab6_correct/HashTable.h
+++ Lab6_correct/HashTable.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <array>
 #include <list>
+#include <vector>
 class Element
 {
 public:
@@ -23,13 +24,19 @@ public:
 
 class HashTable
 {
-	static const int size = 10;
-	std::array<std::list<Element>, size> data;
+	int size = 10;
+	std::vector<std::list<Element>> data;
 
 	int hashFunction(int value);
 public:
+	HashTable(int size)
+	{
+		this->size = size;
+		data = std::vector<std::list<Element>>(size);
+	}
 	void add(int key, int value);
 	Element get(int key);
 	void remove(int key);
 	Element update(int key, int value);
+	void print();
 };
